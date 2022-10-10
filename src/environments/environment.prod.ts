c0f47665550bd65b35ad79ed2f0965503bcacc8c
laurazig <lhzig@protonmail.com>
2022-10-10 09:37:47 +0200

    update name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appName: 'some app',
+  appName: 'Cabl',
   api: {
     url: 'http://localhost:5000/api',
     proxy: 'http://meteor03-prod.meteorit-leipzig.de:5001',
